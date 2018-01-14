c033ebddee3eedf3e41bc045dc46c218f872eee1
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-14 09:27:45 -0500

    Create site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,29 @@
+hiera_include('classes')
+
+# DSpace needs prerequisites before it can be installed, so 'server' class needs loading first
+Class['server'] -> Class['dspace']
+
+##############################
+# Setup all DSpaceDirect sites 
+# (based on Hiera data configuration)
+##############################
+# These next few lines load the Hiera data configs and creates a new "dspace::site"
+# for every site defined under "DSpaceDirect_Sites" in the 'hieradata/[fqdn].yaml' file.
+# Create a hash from Hiera Data with variable values
+$dspacedirect_sites = hiera('DSpaceDirect_Sites', {})   # First read the site configs under "DSpaceDirect_Sites" (default to doing nothing, {}, if nothing is defined under "DSpaceDirect_Sites") 
+create_resources('dspace::site', $dspacedirect_sites)   # Then, create a new "dspace::site" for each one
+
+
+class { 'dspace':}
+
+#owner
+dspace::owner { '$owner':
+  #gid    => 'dspace1',  # Primary OS group name / ID
+  groups => 'root', # Additional OS groups
+  sudoer => true,  # Whether to add acct as a sudoer
+}
+
+#dspace1 install
+
+dspace::install { "/home/${dspace::owner}/dspace" :
+}
