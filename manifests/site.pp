b632556516fe82a114d20859581af2e9b046ae49
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-16 13:36:08 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -46,10 +46,7 @@ create_resources('dspace::site', $dspacedirect_sites)   # Then, create a new "ds
 
 ####dspace1 install
 dspace::install { "/home/${dspace::owner}/dspace" :
-# for debug output on the puppet master
-  notify {"--------------------------------------------------------------------------":}
-  notify {"------------This is to notify us if there is something:$site_name---------":}
-  notify {"--------------------------------------------------------------------------":}
+
 }
 
 }
