2aa1cc600bc3e1940a69befc407c72bcb23c500b
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-19 15:39:19 -0500

    Update pfile.pp

--- manifests/pfile.pp
+++ manifests/pfile.pp
@@ -1,5 +1,7 @@
+class profile{
 define profile:file($title, $content) {
   notify { "The Name of config_file: ${title}": }
   notify { "The content of config_file: ${title} is: ${content}": }
   
+  }
   }
