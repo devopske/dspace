b60fb7f8e01852242dd43004daf10b94ced7ec4d
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-19 18:04:47 -0500

    Update pfile.pp

--- manifests/pfile.pp
+++ manifests/pfile.pp
@@ -1,4 +1,4 @@
-define dspace::filed($title, $content) {
+define dspace::pfile($title, $content) {
   notify { "The Name of config_file: ${title}": }
   notify { "The content of config_file: ${title} is: ${content}": }
   
