95625a2c905dc17f579b6250a63bace4842d70ef
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-19 09:30:11 -0500

    Create gu_misc.pp

---
+++ manifests/gu_misc.pp
@@ -0,0 +1,17 @@
+class gu_misc {
+
+  define mount_nfs_shares(
+        $mount_point,
+        $mount_device) {
+
+        file{ $mout_point: ensure => directory }
+        mount { $mount_point:
+            device => $mount_device,
+            name => $mount_point,
+            require => File[$mount_point],
+        }
+    }
+
+    create_resources(gu_misc::mount_nfs_shares, $nfs_mounts)
+
+}
