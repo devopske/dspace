e5f90a0f8623e771b67c95106f876c68b030c3db
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-19 08:39:36 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -13,10 +13,10 @@ hiera_include('classes')
 
 #class { 'dspace':}
 
-define dspace::site{ 
+#define dspace::site{ 
 
-$dspacedirect_sites = hiera('DSpaceDirect_Sites', {})   # First read the site configs under "DSpaceDirect_Sites" (default to doing nothing, {}, if nothing is defined under "DSpaceDirect_Sites") 
-create_resources('dspace::site', $dspacedirect_sites)   # Then, create a new "dspace::site" for each one
+#$dspacedirect_sites = hiera('DSpaceDirect_Sites', {})   # First read the site configs under "DSpaceDirect_Sites" (default to doing nothing, {}, if nothing is defined under "DSpaceDirect_Sites") 
+#create_resources('dspace::site', $dspacedirect_sites)   # Then, create a new "dspace::site" for each one
 
 ##notice(hiera('DSpaceDirect_Sites')['dspace1.dddke.net'])
 
@@ -33,5 +33,5 @@ dspace::owner { '$owner':
 dspace::install { "/home/${dspace::owner}/dspace" :
 
 }
-}
+#}
 
