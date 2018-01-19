fa75501dcb8e48dcf48a4fc9845229896a5fb8c9
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-19 09:01:52 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,7 +11,7 @@ hiera_include('classes')
 # for every site defined under "DSpaceDirect_Sites" in the 'hieradata/[fqdn].yaml' file.
 # Create a hash from Hiera Data with variable values
 
-#class { 'dspace':}
+class { 'dspace':}
 
 ##dspace::site{ 
 
