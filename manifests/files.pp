9e932c0b67e66ce989827d75886980214ec3912a
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-19 15:38:43 -0500

    Update files.pp

--- manifests/files.pp
+++ manifests/files.pp
@@ -1,7 +1,3 @@
-define profile:file($title, $content) {
-  notify { "The Name of config_file: ${title}": }
-  notify { "The content of config_file: ${title} is: ${content}": }
-  
-  }
+
 $files = hiera_hash('files',{})
 create_resources ( 'profile::file', $files )
