c3949fca73ecf282a40113905bfd10fe2455ece3
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-19 19:35:33 -0500

    Update setup.pp

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -46,58 +46,8 @@ define dspace::setup (
   $tomcat_port        = undef,
 )
 {
-    # Default to requiring all packages be installed
-    Package {
-      ensure => installed,
-    }
-
-    # Install Maven & Ant which are required to build & deploy, respectively
-    # For Maven, do NOT install "recommended" apt-get packages, as this will
-    # install OpenJDK 6 and always set it as the default Java alternative
-    package { 'maven':
-      install_options => ['--no-install-recommends'],
-      before          => Package['java'],
-    }
-    package { "ant":
-      before => Package['java'],
-    }
-
-    # Install Git, needed for any DSpace development
-    package { "git":
-    }
-
-    # Java installation directory
-    $java_install_dir = "/usr/lib/jvm"
-
-    # OpenJDK version/directory name (NOTE: $architecture is a "fact")
-    $java_name = "java-${java_version}-openjdk-${architecture}"
-
-    # Install Java, based on set $java_version
-    package { "java":
-      name => "openjdk-${java_version}-jdk",  # Install OpenJDK package (as Oracle JDK tends to require a more complex manual download & unzip)
-    }
-
- ->
-
-    # Set Java defaults to point at OpenJDK
-    # NOTE: $architecture is a "fact" automatically set by Puppet's 'facter'.
-    exec { "Update alternatives to OpenJDK Java ${java_version}":
-      command => "update-java-alternatives --set ${java_name}",
-      unless  => "test \$(readlink /etc/alternatives/java) = '${java_install_dir}/${java_name}/jre/bin/java'",
-      path    => "/usr/bin:/usr/sbin:/bin",
-    }
-
- ->
-
-    # Create a "default-java" symlink (for easier JAVA_HOME setting). Overwrite if existing.
-    exec { "Symlink OpenJDK to '${java_install_dir}/default-java'":
-      cwd     => $java_install_dir,
-      command => "ln -sfn ${java_name} default-java",
-      unless  => "test \$(readlink default-java) = '${java_name}'",
-      path    => "/usr/bin:/usr/sbin:/bin",
-    }
     
-    #owner
+  #owner
   dspace::owner { '$owner':
   #gid    => 'dspace1',  # Primary OS group name / ID
   groups => 'root', # Additional OS groups
