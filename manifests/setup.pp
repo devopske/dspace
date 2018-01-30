b5ad9b6a4b255751e6afeeedf66b1af25da88b58
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-31 00:40:20 +0300

    Update setup.pp

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -121,6 +121,7 @@ define dspace::setup (
            #hasrestart => true,		# service has a 'restart' command
            use_jsvc => false,
            use_init => true,
+           service_name => $username,
            require    => File["/etc/init.d/${username}"],
          }
       
