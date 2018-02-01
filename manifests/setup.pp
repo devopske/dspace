f9b88345384727794b6f17779787e386e1a60ab9
Billy Nelson <bilnels@users.noreply.github.com>
2018-02-02 01:38:58 +0300

    Update setup.pp

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -70,6 +70,7 @@ define dspace::setup (
                            "/home/${username}/dspace/webapps/solr"    => { path => 'solr' },
                            "/home/${username}/dspace/webapps/sword"   => { path => 'sword' },
                            "/home/${username}/dspace/webapps/swordv2" => { path => 'swordv2' },
+                           }
 
 )
 {
