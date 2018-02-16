c1fc540885b72bab5f01f02aed99f184ae76d84c
dspaceproject <32164818+dspaceproject@users.noreply.github.com>
2018-02-16 15:09:25 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -263,13 +263,13 @@ if $admin_email and $admin_passwd and $admin_firstname and $admin_lastname and $
   # Setup Tomcat Instance
   ##########################
   #Create a new Tomcat instance owned by this user
-  class { 'tomcat':
-    install_from_source => false,
-    }
+#  class { 'tomcat':
+#    install_from_source => false,
+#    }
   
   tomcat::instance { "${owner}" :
            catalina_home => $catalina_home,
-	  #install_from_source => false,
+	   install_from_source => false,
 	  # package_ensure => 'present',
 	   package_name => $tomcat_package,
            #source_url   => $source_url,
